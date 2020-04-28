e48dce7f7fe9549838d6d80cf0f46479ea329ff6
rrangamgari <ravinder.rangamgari@gmail.com>
2020-04-28 12:02:09 -0400

    Icons in the footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,16 @@
 <template>
  <q-footer elevated>
-        <q-toolbar class="glossy">
-      <q-toolbar-title style="align:center">Twitter |Facebook|Instagram|whatshapp</q-toolbar-title>
+        <q-toolbar class="glossy center" >
+      <q-toolbar-title style="align:center;text-align: center;">
+        <img src="../assets/instagram.png" width="40px" height="40px">  &nbsp;
+      <img src="../assets/skype.png" width="40px" height="40px">
+      &nbsp;<img src="../assets/twitter.png" width="40px" height="40px">
+      &nbsp;<img src="../assets/pinint.png" width="40px" height="40px">
+      &nbsp;<img src="../assets/facebook.png" width="40px" height="40px">
+      &nbsp;<img src="../assets/linkedin.png" width="40px" height="40px">
+      &nbsp;<img src="../assets/youtube.png" width="40px" height="40px">
+      &nbsp;<img src="../assets/chat.png" width="40px" height="40px">
+      </q-toolbar-title>
         </q-toolbar>
       </q-footer>
 </template>
