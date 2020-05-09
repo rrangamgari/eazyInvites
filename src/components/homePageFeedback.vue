9250001372b557177967a02f9fb15317993b45d0
Vineeth <Vineeth@Vineeth>
2020-05-09 19:43:20 +0530

    Added Connection to Feedback API

--- src/components/homePageFeedback.vue
+++ src/components/homePageFeedback.vue
@@ -5,154 +5,21 @@
       <div class="q-pa-md">
         <b style="font-size:25px">&nbsp; Our Customer Reviews </b>
         <div class="row q-col-gutter-x-xs q-col-gutter-y-lg">
-          <div class="col-xs-12 col-sm-6 col-md-4">
+          <div class="col-xs-12 col-sm-6 col-md-4" v-for="feedback in feedbackList"
+           :key="feedback.id">
             <div class="my-content" style="padding:20px">
-              <q-rating
-                v-model="ratingModel"
-                size="3.5em"
-                color="grey"
-                :color-selected="ratingColors"
-                icon="star_border"
-                icon-selected="star"
-              />
-              <br />
-              <b style="font-size:20px">Ravinder Rangamgari </b>
-              <p style="font-size:13px">
-                Excellent idea of sending e-invites with whatsapp. Wide range
-                pre designed cards. Used this recently for My son's 12 birthday
-                party. Invitees loved it , it has gps feature for identifying
-                the location with ease...
-                <br />
-                the best part is it send reminders to the guests a day before...
-                <br />
-                <br />
-                Thank you for this awesome product!
-              </p>
-            </div>
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="my-content" style="padding:20px">
-              <q-rating
-                v-model="ratingModel"
-                size="3.5em"
-                color="grey"
-                :color-selected="ratingColors"
-                icon="star_border"
-                icon-selected="star"
-              />
-              <br />
-              <b style="font-size:20px">Sridevi Rangamgari </b>
-              <p style="font-size:13px">
-                Excellent idea of sending e-invites with whatsapp. Wide range
-                pre designed cards. Used this recently for My son's 12 birthday
-                party. Invitees loved it , it has gps feature for identifying
-                the location with ease...
-                <br />
-                the best part is it send reminders to the guests a day before...
-                <br />
-                <br />
-                Thank you for this awesome product!
-              </p>
-            </div>
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="my-content" style="padding:20px">
-              <q-rating
-                v-model="ratingModel"
-                size="3.5em"
-                color="grey"
-                :color-selected="ratingColors"
-                icon="star_border"
-                icon-selected="star"
-              />
-              <br />
-              <b style="font-size:20px">Vivek Rangamgari </b>
-              <p style="font-size:13px">
-                Excellent idea of sending e-invites with whatsapp. Wide range
-                pre designed cards. Used this recently for My son's 12 birthday
-                party. Invitees loved it , it has gps feature for identifying
-                the location with ease...
-                <br />
-                the best part is it send reminders to the guests a day before...
-                <br />
-                <br />
-                Thank you for this awesome product!
-              </p>
-            </div>
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="my-content" style="padding:20px">
-              <q-rating
-                v-model="ratingModel"
-                size="3.5em"
-                color="grey"
-                :color-selected="ratingColors"
-                icon="star_border"
-                icon-selected="star"
-              />
-              <br />
-              <b style="font-size:20px">Divya Molugu </b>
-              <p style="font-size:13px">
-                Excellent idea of sending e-invites with whatsapp. Wide range
-                pre designed cards. Used this recently for My son's 12 birthday
-                party. Invitees loved it , it has gps feature for identifying
-                the location with ease...
-                <br />
-                the best part is it send reminders to the guests a day before...
-                <br />
-                <br />
-                Thank you for this awesome product!
-              </p>
-            </div>
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="my-content" style="padding:20px">
-              <q-rating
-                v-model="ratingModel"
-                size="3.5em"
-                color="grey"
-                :color-selected="ratingColors"
-                icon="star_border"
-                icon-selected="star"
-              />
-              <br />
-              <b style="font-size:20px">Chaitanya Molugu </b>
-              <p style="font-size:13px">
-                Excellent idea of sending e-invites with whatsapp. Wide range
-                pre designed cards. Used this recently for My son's 12 birthday
-                party. Invitees loved it , it has gps feature for identifying
-                the location with ease...
-                <br />
-                the best part is it send reminders to the guests a day before...
-                <br />
-                <br />
-                Thank you for this awesome product!
-              </p>
-            </div>
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-4">
-            <div class="my-content" style="padding:20px">
-              <q-rating
-                v-model="ratingModel"
-                size="3.5em"
-                color="grey"
-                :color-selected="ratingColors"
-                icon="star_border"
-                icon-selected="star"
-              />
-              <br />
-              <b style="font-size:20px">Vineeth Rangamgari </b>
-              <p style="font-size:13px">
-                Excellent idea of sending e-invites with whatsapp. Wide range
-                pre designed cards. Used this recently for My son's 12 birthday
-                party. Invitees loved it , it has gps feature for identifying
-                the location with ease...
-                <br />
-                the best part is it send reminders to the guests a day before...
-                <br />
-                <br />
-                Thank you for this awesome product!
-              </p>
+            <q-rating
+              :value="feedback.rating"
+              size="3.5em"
+              color="grey"
+              color-selected="yellow"
+              icon="star_border"
+              icon-selected="star"
+              readonly
+            />
+            <br>
+            <b style="font-size:18px">{{ feedback.name }}</b>
+            <body style="font-size:12px; white-space: pre-line">{{ feedback.description }}</body>
             </div>
           </div>
         </div>
@@ -161,14 +28,71 @@
   </div>
 </template>
 
+
 <script>
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.BASE_URL;
+axios.defaults.headers.get.Accepts = 'application/json';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
+
 export default {
   // name: 'ComponentName',
   data() {
     return {
-      ratingModel: 5,
-      ratingColors: ['yellow', 'yellow', 'yellow', 'yellow', 'yellow'],
+      feedbackList: [
+        {
+          id: 1,
+          rating: 5,
+          name: 'Ravinder Rangamgari',
+          description: 'Excellent idea of sending e-invites with whatsapp. Wide range pre designed cards. Used this recently for My son\'s 12 birthday party. Invitees loved it , it has gps feature for identifying the location with ease...\nthe best part is it send reminders to the guests a day before...\n\nThank you for this awesome product!',
+        },
+        {
+          id: 1,
+          rating: 5,
+          name: 'Sridevi Rangamgari',
+          description: 'Excellent idea of sending e-invites with whatsapp. Wide range pre designed cards. Used this recently for My son\'s 12 birthday party. Invitees loved it , it has gps feature for identifying the location with ease...\nthe best part is it send reminders to the guests a day before...\n\nThank you for this awesome product!',
+        },
+        {
+          id: 1,
+          rating: 4,
+          name: 'Vivek Rangamgari',
+          description: 'Excellent idea of sending e-invites with whatsapp. Wide range pre designed cards. Used this recently for My son\'s 12 birthday party. Invitees loved it , it has gps feature for identifying the location with ease...\nthe best part is it send reminders to the guests a day before...\n\nThank you for this awesome product!',
+        },
+        {
+          id: 1,
+          rating: 3,
+          name: 'Divya Molugu',
+          description: 'Excellent idea of sending e-invites with whatsapp. Wide range pre designed cards. Used this recently for My son\'s 12 birthday party. Invitees loved it , it has gps feature for identifying the location with ease...\nthe best part is it send reminders to the guests a day before...\n\nThank you for this awesome product!',
+        },
+      ],
     };
+  },
+  mounted() {
+    axios.get('http://localhost:5000/Emantran/api/feedback')
+      .then((response) => {
+        // JSON responses are automatically parsed.
+        this.feedbackList = this.feedbackList.concat(response.data.data);
+        // Notification for testing api
+        this.$q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'cloud_done',
+          message: response.data.message,
+          position: 'center',
+        });
+      })
+      .catch((e) => {
+      //  this.errors.push(e);
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'error',
+          message: e.message,
+          position: 'top',
+        });
+      });
   },
 };
 </script>
