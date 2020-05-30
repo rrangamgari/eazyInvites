3a36e00f0a8aa504561bb5105c452fa0b569f1a5
Vineeth <vineeth.kumar2941@gmail.com>
2020-05-30 18:07:54 +0530

    Minute Change

--- src/layouts/eventStatusLayout.vue
+++ src/layouts/eventStatusLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <q-layout view="hHh lpr fff">
 
+    <header-component></header-component>
     <footer-component></footer-component>
 
     <q-page-container>
@@ -13,12 +14,14 @@
 
 <script>
 import statusComponent from '../components/status.vue';
+import headerComponent from '../components/header.vue';
 import footerComponent from '../components/footer.vue';
 
 export default {
-  name: 'staus',
+  name: 'eventStatus',
   components: {
     statusComponent,
+    headerComponent,
     footerComponent,
   },
 };
