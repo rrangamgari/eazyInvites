6de849aa7333727ffe83435bc71665fd90129978
rrangamgari <ravinder.rangamgari@gmail.com>
2020-05-31 15:45:43 -0400

    UI Update

--- src/components/homePageCarousel.vue
+++ src/components/homePageCarousel.vue
@@ -51,7 +51,7 @@
           <div class="text-subtitle1 merienda">Once is Lifetime</div>
         </div>
       </q-carousel-slide>
-    <q-carousel-slide name="third" img-src="~assets/home/ez1.jpg" >
+    <q-carousel-slide name="third" img-src="~assets/home/ez33.jpg" >
       <Snowf
         class="overlay"
         :amount="30"
@@ -77,7 +77,7 @@
           <div class="text-subtitle1 merienda">Engagement, Wedding, Receptions etc</div>
         </div>
       </q-carousel-slide>-->
-       <q-carousel-slide name="first1" img-src="~assets/home/ez22.jpg" >
+       <q-carousel-slide name="first1" img-src="~assets/home/ez15.jpg" >
          <Snowf
            class="overlay"
            :amount="30"
@@ -91,11 +91,12 @@
            :resize="false"
            color="#FFFF00"
          />
-        <div class="absolute-right custom-caption">
-          <div class="text-h2 Dancing">Festivals</div>
-          <div class="text-h4">&nbsp;</div>
-          <div class="text-subtitle1 merienda">Christmas Party</div>
-        </div>
+         <div class="absolute-bottom custom-caption">
+           <div class="text-h2 Dancing">New Baby Born</div>
+           <div class="text-h4">&nbsp;</div>
+           <div class="text-subtitle1 merienda">
+             “Children bring us a piece of heaven on earth.” – Roland Leonhardt</div>
+         </div>
       </q-carousel-slide>
       <!--<q-carousel-slide name="second1" img-src="../assets/home/ez15.jpg">
         <div class="absolute-bottom custom-caption">
