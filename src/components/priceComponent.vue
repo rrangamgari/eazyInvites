a790fb0d0b3d6ab146d566597b65f172fcbfc8d7
rrangamgari <ravinder.rangamgari@gmail.com>
2020-07-05 19:32:07 -0400

    Pricing part is done

--- src/components/priceComponent.vue
+++ src/components/priceComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-grey-2 q-pa-md">
     <div class="row justify-center q-gutter-lg flex flex-center">
-      <q-card>
+      <!--<q-card>
             <q-card-section class="text-center price_header">
               <p class="price_header_text">Free Plan</p>
             </q-card-section>
@@ -59,8 +59,8 @@
             <q-card-section class="flex flex-center"
             >
             <div>
-               <!--<a href="#" class="btn btn-large btn-green btn-green-margin"
-              >Select Plan</a>-->
+               &lt;!&ndash;<a href="#" class="btn btn-large btn-green btn-green-margin"
+              >Select Plan</a>&ndash;&gt;
               <q-btn label="Select Plan" type="button" color="primary" rounded=""/>
           </div>
             </q-card-section>
@@ -89,7 +89,7 @@
             <q-btn label="Select Plan" type="button" color="primary" rounded=""/>
           </div>
         </q-card-section>
-      </q-card>
+      </q-card>-->
        <!-- <q-card>
             <q-card-section class="text-center price_header">
               <p class="price_header_text">Premium Plan</p>
@@ -139,6 +139,77 @@
             </q-card-section>
           </q-card>-->
     </div>
+    <section id="pricing" class="pricing section-bg">
+      <div class="container">
+        <div class="section-header" >
+          <h3 style="font-size: 32px;">Pricing</h3>
+          <p>Our Pricing plan is pretty simple pay as you go...</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-xs-12 col-sm-6 col-md-4">
+            <div class="box">
+              <h3>Free Plan</h3>
+              <h4><sup>$</sup>0<span> / Invitation</span></h4>
+              <ul>
+                <li>Email Invitation is always free</li>
+                <li>Track your invitations </li>
+                <li>* We will not share your emails</li>
+                <li class="na"></li>
+                <li class="na"></li>
+                <div class="btn-wrap">
+                  <a href="#" class="btn-buy">Get Started</a>
+                </div>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-4">
+            <div class="box featured">
+              <h3>Premium Plan</h3>
+              <h4><span> Starts from </span><sup>$</sup>9.99<span> / Invitation</span></h4>
+              <ul>
+                <li>Premium Package includes emails and Whatsapp invitations</li>
+                <li>Upto 100 Email Invitations</li>
+                <li>Upto 100 Whatsapp Invitations</li>
+                <li></li>
+                <div class="btn-wrap">
+                  <a href="#" class="btn-buy">Get Started</a>
+                </div>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-4">
+            <div class="box">
+              <h3>Super Premium Plan</h3>
+              <h1>
+                <sup>$</sup>
+                <abbr>{{Number(Math.round(standard *9.99)/100).toFixed(2)}}</abbr>
+                <span>&nbsp;  / Actual Price</span></h1>
+              <h4><sup>$</sup>
+                <abbr>{{Math.round((standard *.09)*.9)+.99}} </abbr>
+                <span>/ Discount Price</span></h4>
+              <ul>
+                <li>Custom Calculation</li>
+                <li><q-badge color="primary">
+                  {{ standard }} Invitations
+                </q-badge>
+                  <q-slider v-model="standard" :min="200" :max="5000"
+                            :step="100"
+                            label/></li>
+              </ul>
+              <div class="btn-wrap">
+                <a href="#" class="btn-buy">Get Started</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Pricing Section -->
   </div>
 </template>
 
