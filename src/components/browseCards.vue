f27ac3df7aa70c4cd4bc68b3b0f2c35235774fd5
rkvivek97 <rkvivek97@gmail.com>
2020-07-18 17:39:36 +0530

    Changed browsecards layout with preview options. Also made the my event's cards uniform.

--- src/components/browseCards.vue
+++ src/components/browseCards.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="q-pa-lg row warp justify-left items-center">
+    <div class="q-pa-lg row warp justify-left items-center" style="background: #f7f7f7;">
       <div class="col-12 q-px-md q-py-sm row">
         <q-select class="col-8" style="max-width: 300px;" label="Event Type"
          outlined v-model="eventType" :options="eventTypeOptions"/>
@@ -10,14 +10,31 @@
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 q-px-md q-py-sm"
        v-for="index in indices" :key="index">
-        <q-card @click="selected === index ? selected = null : selected = index;"
-         :style="selected === index ? `outline: 3px solid ${primary};
-          border-radius: 0px; margin: -8px;` : ''">
-          {{index}}
-            <q-img class="q-pa-xs" :ratio="16/9" width="100%"
-             :src="require(`../assets/home/ez${index}.jpg`)" />
-        </q-card>
+            <section id="portfolio"  class="section-bg" >
+              <div class="col-lg-4 col-md-6 portfolio-item filter-card wow fadeInUp">
+                <div class="portfolio-wrap">
+                  <figure>
+                    <img height="250px" :ratio="16/9" style="width: 100%;"
+                     :src="require(`../assets/home/ez${index}.jpg`)" >
+                    <a @click= "showCard(require(`../assets/home/ez${index}.jpg`))"
+                    data-lightbox="portfolio" data-title="Card"
+                    class="link-preview" title="Preview"><i class="ion ion-eye"></i></a>
+                    <a  @click= "$router.push(`/editcard/${index}`)"
+                     class="link-details" title="Use this card">
+                    <i class="ion ion-android-open"></i></a>
+                  </figure>
+                  <div class="portfolio-info">
+                    <h4>{{index}}</h4>
+                  </div>
+                </div>
+              </div>
+            </section>
       </div>
+      <VueEasyLightbox
+        :visible="visible"
+        :imgs="imgs"
+        @hide="handleHide"
+      ></VueEasyLightbox>
     </div>
   </div>
 </template>
@@ -25,6 +42,7 @@
 <script>
 import { Loading, QSpinnerBars, colors } from 'quasar';
 import axios from 'axios';
+import VueEasyLightbox from 'vue-easy-lightbox';
 
 axios.defaults.baseURL = process.env.BASE_URL;
 axios.defaults.headers.get.Accepts = 'application/json';
@@ -32,6 +50,9 @@ axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
 
 export default {
+  components: {
+    VueEasyLightbox,
+  },
   name: 'statusComponent',
   data() {
     return {
@@ -40,6 +61,8 @@ export default {
       indices: [11, 12, 13, 14, 15, 16, 17, 20, 21, 22, 23],
       selected: null,
       primary: '',
+      imgs: '',
+      visible: false,
     };
   },
   mounted() {
@@ -78,6 +101,13 @@ export default {
       });
   },
   methods: {
+    showCard(src) {
+      this.imgs = src;
+      this.visible = true;
+    },
+    handleHide() {
+      this.visible = false;
+    },
   },
 };
 </script>
